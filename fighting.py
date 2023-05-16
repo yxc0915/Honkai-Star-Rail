c9cda3ad4db6b093da009b9fbdcf512f3d9d003c
yxc0915 <62410385+yxc0915@users.noreply.github.com>
2023-05-16 09:32:03 +0800

    Update fighting.py

--- fighting.py
+++ fighting.py
@@ -6,12 +6,20 @@ def fight():
         path = [tuple(map(int, line.strip().split(','))) for line in f.readlines()]for pos in path:
         pyautogui.moveTo(pos)
         time.sleep(0.1)
-        pyautogui.click()# 自动打怪
-        while True:
-            if pyautogui.locateOnScreen('enemy.png', confidence=0.9) is not None:
-                # 找到敌人，自动进入战斗
-                pyautogui.click()
-                time.sleep(1)
-            else:
-                # 没有敌人，自动寻路
-                break
+        if pyautogui.locateOnScreen('enemy1.png', confidence=0.9) is not None:
+            # 找到敌人1，自动进入战斗
+            fight_enemy1()
+            time.sleep(1)
+        elif pyautogui.locateOnScreen('enemy2.png', confidence=0.9) is not None:
+            # 找到敌人2，自动进入战斗
+            fight_enemy2()
+            time.sleep(1)
+        else:
+            # 没有敌人，自动寻路
+            breakdef fight_enemy1():
+    # 自动打敌人1
+    pyautogui.click()
+    time.sleep(1)def fight_enemy2():
+    # 自动打敌人2
+    pyautogui.click()
+    time.sleep(1)
